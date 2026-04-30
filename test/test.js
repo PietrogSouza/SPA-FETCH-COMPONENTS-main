ed9319e463729437b0852e9b70e2fbd46a26b69f
Pietro <pietro.g.souza@aluno.senai.br>
2026-04-30 11:10:33 -0300

    Abstração do código da requisição HTTP com fetch e .then

--- test/test.js
+++ test/test.js
@@ -118,15 +118,37 @@
 //-------------------------------------------------------------------------------------------------------------//
 
 
-fetch('https://viacep.com.br/ws/01001000/json/').then(resposta => resposta.json()).then(dados => console.log(dados))
+// fetch('https://viacep.com.br/ws/01001000/json/').then(resposta => resposta.json()).then(dados => console.log(dados))
 
-fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json()) // converte o JSON para objeto JavaScript e estrai os dados
-.then(dados => console.log(dados)) // exibe os dados extraídos
-.catch(error => console.warn(error))
 
-const consulta = fetch('https://viacep.com.br/ws/01001000/json/')
-console.log(consulta);
+const url = 'https://viacep.com.br/ws/01001000/json/'
+const consulta = fetch(url);
+
+consulta.then((resposta) => {
+
+    if(!resposta.ok){
+        throw new Error('Cep Inválido')
+    }
+    const resp = resposta.json()
+    console.log(resp)
+    return resp
+
+})
+.then((dados) => {
+
+    console.log(dados)
+
+})
+.catch((error) => {
+    
+    console.warn(error)
+
+})
+
+// const consulta = fetch('https://viacep.com.br/ws/01001000/json/')
+// console.log(consulta);
+
+
 
 
 
