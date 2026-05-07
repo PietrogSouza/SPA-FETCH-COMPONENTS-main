d2cb554be83e19eced53a80ca111b89a48e3448e
Pietro <pietro.g.souza@aluno.senai.br>
2026-05-07 09:44:12 -0300

    Adicionando um evento aos botões e mostrando o cep no console

--- test/cep/script-cep.js
+++ test/cep/script-cep.js
@@ -1,23 +1,43 @@
+const inputCep = document.querySelector('input__cep')
+const botaoThen = document.querySelector('buscarComThen')
+const botaoAsync = document.querySelector('buscarComAsync')
+
 const url = 'https://viacep.com.br/ws/01001000/json/'
 const consulta = fetch(url);
 
-consulta.then((resposta) => {
+botaoThen.addEventListener("click", function(){
+    consulta
+    .then((response) => {
 
-    if(!resposta.ok){
-        throw new Error('Cep Inválido')
-    }
-    const resp = resposta.json()
-    console.log(resp)
-    return resp
+        if(!response.ok){
+            throw new Error('Erro na Requisição.')
+        }
+        return response.json()
+    })
+    .then((dados) => {
 
+        if(dados.erro){
+            throw new Error('CEP inválido ou não encontrado.')
+        }
+        console.log(dados)
+    })
+    .catch((erro) => {
+        console.warn(erro.message)
+    });
 })
-.then((dados) => {
 
-    console.log(dados)
 
-})
-.catch((error) => {
-    
-    console.warn(error)
+//---------------------------------------------------------------------------------//
 
+
+/* Criar uma requisição HTTP com fecth e async/await */
+async function buscarCep(){
+    let resposta = await consulta;
+    console.log(resposta)
+    let dataObj = await resposta.json();
+    console.log(dataObj);
+};
+
+botaoAsync.addEventListener("click", function(){
+    buscarCep();
 })
